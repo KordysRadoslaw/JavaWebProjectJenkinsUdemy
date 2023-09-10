27ca4041b330d7ae3091e415c1d695d1566b864e
KordysRadoslaw <40690745+KordysRadoslaw@users.noreply.github.com>
2023-09-10 22:58:02 +0200

    Add files via upload

---
+++ src/test/java/com/example/javawebprojectjenkinsudemy/JavaWebProjectJenkinsUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javawebprojectjenkinsudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebProjectJenkinsUdemyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
